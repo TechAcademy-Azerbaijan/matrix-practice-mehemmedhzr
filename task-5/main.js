0753e9644f1c3ed16cd2ef400bd8cd722dd5923f
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-06-17 00:56:39 -0700

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -2,8 +2,14 @@ const prompt = require('prompt');
 prompt.start();
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+prompt.get('input', function (err, result){
+    let verilen = result.input
+    let kvadrat = ''
+    for(let i = 1; i <= verilen; i++){
+        for(let j = 1; j <= verilen; j++){
+            kvadrat += '*'
+        }
+        kvadrat += '\n'
+    }
+    console.log(kvadrat);
+})
